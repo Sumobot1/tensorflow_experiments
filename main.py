87933700b0c77255af9350d344364475bd474c2c
Michael Peet <ubernopeavi@gmail.com>
2018-05-18 16:40:18 -0400

    Added smaller cnn for pipeline work on laptop, added functions to clean up models file

--- main.py
+++ main.py
@@ -5,16 +5,17 @@ import multiprocessing as mp
 import glob
 import os
 from data_pipeline import generate_tfrecords, imgs_input_fn
-from models import cnn_model_fn
+from models import cnn_model_fn, fast_cnn_model_fn
 
 
 def main(argv):
+    machine_type = 'laptop' if '--laptop' in argv else 'desktop'
     # Need to set logging verbosity to INFO level or training loss will not print
     tf.logging.set_verbosity(tf.logging.INFO)
     # Training data needs to be split into training, validation, and testing sets
     # This needs to be a complete (not relative) path, or glob will run into issues
 
-    cat_dog_train_path = '/home/michael/Documents/DataSets/dogs_vs_cats_data/*.jpg' if '--laptop' in argv else '/home/michael/hard_drive/datasets/dogs_vs_cats_data/train/*.jpg'
+    cat_dog_train_path = '/home/michael/Documents/DataSets/dogs_vs_cats_data/*.jpg' if machine_type == 'laptop' else '/home/michael/hard_drive/datasets/dogs_vs_cats_data/train/*.jpg'
     if '--generate_tfrecords' in sys.argv:
         for file in glob.glob('*.tfrecords'):
             os.remove(file)
@@ -24,16 +25,18 @@ def main(argv):
     
     # A good way to debug programs like this is to run a tf.InteractiveSession()
     # sess = tf.InteractiveSession()
-
-    mnist_classifier = tf.estimator.Estimator(model_fn=cnn_model_fn, model_dir="/tmp/mnist_convnet_model5")
+    model_fn = fast_cnn_model_fn if machine_type == 'laptop' else cnn_model_fn
+    if not os.path.exists('models'):
+        os.makedirs('models')
+    mnist_classifier = tf.estimator.Estimator(model_fn=model_fn, model_dir="models/cat_dog_cnn_{}".format(machine_type))
     tensors_to_log = {"probabilities": "ting"}
     logging_hook = tf.train.LoggingTensorHook(tensors=tensors_to_log, every_n_iter=50)
 
-    training_batch_size = 1 if '--laptop' in argv else 20
+    training_batch_size = 1 if machine_type == 'laptop' else 20
     train_records = glob.glob('train*.tfrecords')
     train_records.sort()
-
-    mnist_classifier.train(input_fn=lambda: imgs_input_fn(train_records, 'train', perform_shuffle=True, repeat_count=500, batch_size=training_batch_size), steps=50000, hooks=[logging_hook])
+    # Steps is how many times to call next on the input function - ie how many batches to take in?
+    mnist_classifier.train(input_fn=lambda: imgs_input_fn(train_records, 'train', perform_shuffle=True, repeat_count=1, batch_size=training_batch_size), steps=10, hooks=[logging_hook])
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -1,5 +1,27 @@
 import tensorflow as tf
 
+
+def tf_model_estimator(logits, labels, predictions, mode):
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+    # Note:
+    # tf.losses.sparse_softmax_cross_entropy is depricated and will be removed soon
+    # tf.nn.sparse_softmax_cross_entropy_with_logits works differently and returns a tensor instead of a differentiable value.
+    # It needs to be wrapped in tf.reduce_mean to work properly
+
+    # loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+    loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits), name='ting')
+    # Configure the Training Op (for TRAIN mode)
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+        train_op = optimizer.minimize(loss=loss, global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+    # Add evaluation metrics (for EVAL mode)
+    eval_metric_ops = {"accuracy": tf.metrics.accuracy(labels=labels, predictions=predictions["classes"])}
+    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
 def cnn_model_fn(features, labels, mode):
     """Model function for CNN."""
     # Convolutional Layer #1
@@ -24,23 +46,25 @@ def cnn_model_fn(features, labels, mode):
       "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
     }
 
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+    return tf_model_estimator(logits, labels, predictions, mode)
 
-    # Note:
-    # tf.losses.sparse_softmax_cross_entropy is depricated and will be removed soon
-    # tf.nn.sparse_softmax_cross_entropy_with_logits works differently and returns a tensor instead of a differentiable value.
-    # It needs to be wrapped in tf.reduce_mean to work properly
 
-    # loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
-    loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits), name='ting')
+def fast_cnn_model_fn(features, labels, mode):
+    """Model function for CNN."""
+    # Convolutional Layer #1
+    conv1 = tf.layers.conv2d(inputs=features, filters=32, kernel_size=[3, 3], padding="valid", activation=tf.nn.relu)
+    # Pooling Layer #1
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+    # Dense Layer
+    pool1_flat = tf.layers.flatten(inputs=pool1)
+    # Logits Layer
+    logits = tf.layers.dense(inputs=pool1_flat, units=2)
+    predictions = {
+      # Generate predictions (for PREDICT and EVAL mode)
+      "classes": tf.argmax(input=logits, axis=1),
+      # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
+      # `logging_hook`.
+      "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+    }
 
-    # Configure the Training Op (for TRAIN mode)
-    if mode == tf.estimator.ModeKeys.TRAIN:
-        optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
-        train_op = optimizer.minimize(loss=loss, global_step=tf.train.get_global_step())
-        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
-
-    # Add evaluation metrics (for EVAL mode)
-    eval_metric_ops = {"accuracy": tf.metrics.accuracy(labels=labels, predictions=predictions["classes"])}
-    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+    return tf_model_estimator(logits, labels, predictions, mode)
