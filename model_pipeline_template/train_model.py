49ea01ac372ef9f437e4440e75364d3a3bc5183a
Michael Peet <ubernopeavi@gmail.com>
2018-11-13 23:08:35 -0500

    Initialized all random seeds to the same number every run to make runs (kind of) reproducible
    (cherry picked from commit 7cce1f2bed78dad8acc8c57a11ac12e64e872de1)

--- model_pipeline_template/train_model.py
+++ model_pipeline_template/train_model.py
@@ -9,6 +9,7 @@ import numpy as np
 import time
 import json
 from termcolor import cprint
+import random as rn
 
 from functools import reduce
 from model_pipeline_utils.data_pipeline import generate_tfrecords, imgs_input_fn, get_tfrecords, clear_old_tfrecords, clean_model_dir, create_val_dir
@@ -31,6 +32,8 @@ DATA_REPETITIONS_PER_EPOCH = 1
 
 
 def main(clean_dir, num_epochs, val_start_epoch, summary_start_epoch, train_batch_size, config_file, model_dir, model_name, model_file, val_batch_size):
+    np.random.seed(1)
+    rn.seed(1)
     # tf.logging.set_verbosity(tf.logging.INFO)
     tf.logging.set_verbosity(tf.logging.WARN)
     validation_save_path = create_val_dir()
@@ -45,33 +48,56 @@ def main(clean_dir, num_epochs, val_start_epoch, summary_start_epoch, train_batc
     model_fn = get_appropriate_model(model_name)
 
     tf.reset_default_graph()
-    sess = tf.InteractiveSession()
-    next_example, next_label = imgs_input_fn(train_records, 'train', input_dims, output_dims, perform_shuffle=True, repeat_count=-1, batch_size=train_batch_size)
-    next_val_example, next_val_label = imgs_input_fn(val_records, 'val', input_dims, output_dims, perform_shuffle=False, repeat_count=-1, batch_size=val_batch_size)
-    image_batch, label_batch = get_io_placeholders(next_example, next_label, input_dims, output_dims, "image_batch", "label_batch")
-    image_val_batch, label_val_batch = get_io_placeholders(next_val_example, next_val_label, input_dims, output_dims, "image_val_batch", "label_val_batch")
-    is_train = tf.placeholder_with_default(False, shape=(), name="is_training")
-    final_dropout_rate = tf.placeholder_with_default(0.9, shape=[], name="final_dropout_rate")
-    # tf.placeholder_with_default(next_example, shape=input_dims)
-    # Cannot change histogram summary and then reload model from the same checkpoint
-    # TODO: Get rid of unneeded params
-    loss, predictions = model_fn(image_batch, label_batch, is_train, final_dropout_rate, params={"return_estimator": False, "histogram_summary": False, "loss_summary": True, "use_dropout": True})
-    optimizer = tf.train.AdamOptimizer()
-    training_op = optimizer.minimize(loss, name="training_op")
-    num_steps = get_num_steps(train_record_lengths, train_batch_size, DATA_REPETITIONS_PER_EPOCH)
-    print("Train record lengths: {}".format(train_record_lengths))
-    print("Val record lengths: {}".format(val_record_lengths))
-    num_val_steps = get_num_steps(val_record_lengths, val_batch_size, 1)
-    if num_val_steps == 0:
-        cprint("Batch size is larger than number of validation records.  Please decrease val_batch_size", "red")
-        return
-    os.makedirs("data/tf_summaries/train", exist_ok=True)
-    os.makedirs("data/tf_summaries/val", exist_ok=True)
+    with tf.Graph().as_default():
+        tf.set_random_seed(1)
+        with tf.Session() as sess:
+            # sess = tf.InteractiveSession()
+            next_example, next_label = imgs_input_fn(train_records, 'train', input_dims, output_dims, perform_shuffle=False, repeat_count=-1, batch_size=train_batch_size)
+            next_val_example, next_val_label = imgs_input_fn(val_records, 'val', input_dims, output_dims, perform_shuffle=False, repeat_count=-1, batch_size=val_batch_size)
+            image_batch, label_batch = get_io_placeholders(next_example, next_label, input_dims, output_dims, "image_batch", "label_batch")
+            image_val_batch, label_val_batch = get_io_placeholders(next_val_example, next_val_label, input_dims, output_dims, "image_val_batch", "label_val_batch")
+            is_train = tf.placeholder_with_default(False, shape=(), name="is_training")
+            final_dropout_rate = tf.placeholder_with_default(0.9, shape=[], name="final_dropout_rate")
+            # tf.placeholder_with_default(next_example, shape=input_dims)
+            # Cannot change histogram summary and then reload model from the same checkpoint
+            # TODO: Get rid of unneeded params
+            loss, predictions = model_fn(image_batch, label_batch, is_train, final_dropout_rate, params={"return_estimator": False, "histogram_summary": False, "loss_summary": True, "use_dropout": True})
+            optimizer = tf.train.AdamOptimizer()
+            training_op = optimizer.minimize(loss, name="training_op")
+            num_steps = get_num_steps(train_record_lengths, train_batch_size, DATA_REPETITIONS_PER_EPOCH)
+            print("Train record lengths: {}".format(train_record_lengths))
+            print("Val record lengths: {}".format(val_record_lengths))
+            num_val_steps = get_num_steps(val_record_lengths, val_batch_size, 1)
+            if num_val_steps == 0:
+                cprint("Batch size is larger than number of validation records.  Please decrease val_batch_size", "red")
+                return
+            os.makedirs("data/tf_summaries/train", exist_ok=True)
+            os.makedirs("data/tf_summaries/val", exist_ok=True)
 
-    merged = tf.summary.merge_all()
-    train_writer = tf.summary.FileWriter('data/tf_summaries/train', sess.graph)
-    test_writer = tf.summary.FileWriter('data/tf_summaries/val')
-    train_model(sess, num_steps, num_epochs, image_batch, label_batch, loss, predictions, training_op, num_val_steps, image_val_batch, label_val_batch, validation_save_path, merged, train_writer, test_writer, ckpt_path, model_dir, val_start_epoch, summary_start_epoch, is_train, final_dropout_rate)
+            merged = tf.summary.merge_all()
+            train_writer = tf.summary.FileWriter('data/tf_summaries/train', sess.graph)
+            test_writer = tf.summary.FileWriter('data/tf_summaries/val')
+            train_model(sess,
+                        num_steps,
+                        num_epochs,
+                        image_batch,
+                        label_batch,
+                        loss,
+                        predictions,
+                        training_op,
+                        num_val_steps,
+                        image_val_batch,
+                        label_val_batch,
+                        validation_save_path,
+                        merged,
+                        train_writer,
+                        test_writer,
+                        ckpt_path,
+                        model_dir,
+                        val_start_epoch,
+                        summary_start_epoch,
+                        is_train,
+                        final_dropout_rate)
 
 
 if __name__ == "__main__":
