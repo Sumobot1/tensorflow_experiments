9b595b444ceabb8f62697d437afbe921e7c733fc
Michael Peet <ubernopeavi@gmail.com>
2018-11-26 22:36:25 -0500

    Changed yolov3 to a model to keep track of layers better, finished yolov3 body

--- model_pipeline_template/model_pipeline_utils/tf_models/abstract_layers.py
+++ model_pipeline_template/model_pipeline_utils/tf_models/abstract_layers.py
@@ -96,18 +96,40 @@ def softmax_classifier_output(logits, labels, predictions, mode, params):
                    lambda: (mean_softmax_cross_entropy_with_logits(labels, logits, params), predictions),
                    lambda: (tf.constant(0, dtype=tf.float32), predictions))
 
+
 def leaky_relu_layer(input_layer, layer_name, alpha=0.3, summary=False):
     layer = tf.nn.leaky_relu(input_layer, alpha=alpha)
     _layer_histogram(layer_name, layer, summary)
     return layer
 
-def upsampling_2d_layer(input_layer, layer_name, summary=False):
+
+def upsampling_2d_layer(input_layer, multiplier, layer_name, summary=False):
     input_dims = input_layer.shape.as_list()
-    layer = tf.image.resize_nearest_neighbor(input_layer, (2*input_dims[1], 2*input_dims[2]))
+    layer = tf.image.resize_nearest_neighbor(input_layer, (multiplier*input_dims[1], multiplier*input_dims[2]))
     _layer_histogram(layer_name, layer, summary)
     return layer
+
 
 def concat_layer(values, axis, layer_name, summary=False):
     layer = tf.concat(values, axis, name=layer_name)
     _layer_histogram(layer_name, layer, summary)
     return layer
+
+def l1_l2_reg(l1=0.0, l2=0.0):
+        return tf.keras.regularizers.L1L2(l1, l2)
+
+    # ting = {"input_layer": params[0], "num_filters": params[1], "kernel_size": params[2], "layer_name": "ya", "use_bias": params[4], "strides": params[5]}
+    # self.layers.append(layer(**ting))
+
+def darknet_conv2d(input_layer, num_filters, kernel_size, layer_name, use_bias=True, strides=(1, 1), summary=False):
+    padding = 'valid' if strides == (2, 2) else 'same'
+    return conv_2d_layer(input_layer, num_filters, kernel_size, padding, None, 'glorot_uniform', layer_name,
+                         strides=strides, kernel_regularizer=l1_l2_reg(l2=5e-4), use_bias=use_bias, summary=summary)
+
+def padding_layer(input_layer, paddings, mode, layer_name, summary=False):
+    layer = tf.pad(input_layer, paddings=paddings, mode=mode, name=layer_name)
+    _layer_histogram(layer_name, layer, summary)
+    return layer
+
+def add_layer(layers):
+    return tf.keras.layers.add(layers)
--- model_pipeline_template/model_pipeline_utils/tf_models/yolo_v3.py
+++ model_pipeline_template/model_pipeline_utils/tf_models/yolo_v3.py
@@ -1,72 +1,106 @@
 import tensorflow as tf
-from model_pipeline_utils.tf_models.abstract_layers import conv_2d_layer, batch_norm_layer, leaky_relu_layer, upsampling_2d_layer, concat_layer
+from model_pipeline_utils.tf_models.abstract_layers import conv_2d_layer, batch_norm_layer, leaky_relu_layer,\
+    upsampling_2d_layer, concat_layer, l1_l2_reg, darknet_conv2d, padding_layer, add_layer
 
 
-def l1_l2_reg(l1=0.0, l2=0.0):
-    return tf.keras.regularizers.L1L2(l1, l2)
+class YOLOV3:
+    def __init__(self):
+        self.layers = []
+
+    def append_layer(self, layer):
+        self.layers.append(layer)
+        return layer
+
+    def darknet_conv2d(self, input_layer, num_filters, kernel_size, layer_name, use_bias=True, strides=(1, 1), summary=False):
+        return self.append_layer(darknet_conv2d(input_layer, num_filters, kernel_size, layer_name, use_bias, strides, summary))
+
+    def batch_norm_layer(self, input_layer, axis_num, layer_name, summary=False):
+        return self.append_layer(batch_norm_layer(input_layer, axis_num, layer_name, summary=summary))
+
+    def leaky_relu_layer(self, input_layer, layer_name, alpha=0.3, summary=False):
+        return self.append_layer(leaky_relu_layer(input_layer, layer_name, alpha=alpha, summary=summary))
+
+    def padding_layer(self, input_layer, paddings, mode, layer_name, summary=False):
+        return self.append_layer(padding_layer(input_layer, paddings, mode, layer_name, summary=summary))
+
+    def upsampling_2d_layer(self, input_layer, multiplier, layer_name, summary=False):
+        return self.append_layer(upsampling_2d_layer(input_layer, multiplier, layer_name, summary))
+
+    def add_layer(self, layers):
+        return self.append_layer(add_layer(layers))
+
+    def concat_layer(self, values, axis, layer_name, summary=False):
+        return self.append_layer(concat_layer(values, axis, layer_name, summary))
+
+    def darknet_conv2d_bn_leaky(self, input_layer, num_filters, kernel_size, strides=(1, 1), layer_num=None):
+        darknet_conv = self.darknet_conv2d(input_layer, num_filters, kernel_size, "darknet_conv_{}".format(layer_num), False, strides)
+        batch_norm = self.batch_norm_layer(darknet_conv, -1, "batch_norm_{}".format(layer_num), summary=False)
+        return self.leaky_relu_layer(batch_norm, "leaky_relu_{}".format(layer_num), alpha=0.3, summary=False)
+
+    def resblock_body(self, input_layer, num_filters, num_blocks, layer_num):
+        zero_pad = self.padding_layer(input_layer, paddings=((0, 0), (1, 0), (1, 0), (0, 0)), mode="CONSTANT", layer_name="pad_{}".format(layer_num))
+        print(zero_pad)
+        # zero_pad = tf.keras.layers.ZeroPadding2D()
+        leaky_darknet = self.darknet_conv2d_bn_leaky(zero_pad, num_filters, (3, 3), (2, 2), "{}_start".format(layer_num))
+        print(leaky_darknet)
+        current_network = leaky_darknet
+        for i in range(num_blocks):
+            conv_block = self.darknet_conv2d_bn_leaky(current_network, num_filters // 2, (1, 1), layer_num="{}_part_{}".format((layer_num + i), 1))
+            conv_block = self.darknet_conv2d_bn_leaky(conv_block, num_filters, (3, 3), layer_num="{}_part_{}".format((layer_num + i), 2))
+            current_network = self.add_layer([current_network, conv_block])
+            print(current_network)
+        return current_network
+
+    def darknet_body(self, image_input):
+        x = self.darknet_conv2d_bn_leaky(image_input, 32, (3, 3), layer_num=0)
+        x = self.resblock_body(x, 64, 1, layer_num=1)
+        x = self.resblock_body(x, 128, 2, layer_num=2)
+        x = self.resblock_body(x, 256, 8, layer_num=4)
+        x = self.resblock_body(x, 512, 8, layer_num=12)
+        x = self.resblock_body(x, 1024, 4, layer_num=20)
+        return x
+
+    def make_last_layers(self, input_layer, num_filters, num_outputs, output_identifier):
+        x = self.darknet_conv2d_bn_leaky(input_layer, num_filters, (1, 1), layer_num="x_{}_output_layer_{}".format(output_identifier, 0))
+        x = self.darknet_conv2d_bn_leaky(x, num_filters * 2, (3, 3), layer_num="x_{}_output_layer_{}".format(output_identifier, 1))
+        x = self.darknet_conv2d_bn_leaky(x, num_filters, (1, 1), layer_num="x_{}_output_layer_{}".format(output_identifier, 2))
+        x = self.darknet_conv2d_bn_leaky(x, num_filters * 2, (3, 3), layer_num="x_{}_output_layer_{}".format(output_identifier, 3))
+        x = self.darknet_conv2d_bn_leaky(x, num_filters, (1, 1), layer_num="x_{}_output_layer_{}".format(output_identifier, 4))
+
+        y = self.darknet_conv2d_bn_leaky(x, num_filters * 2, (3, 3), layer_num="y_{}_output_layer_{}".format(output_identifier, 0))
+        y = self.darknet_conv2d(y, num_outputs, (1, 1), layer_name="y_{}_output_layer_{}".format(output_identifier, 1))
+        return x, y
+
+    def yolo_body(self, image_input, num_anchors, num_classes):
+        """
+        image_input: [None, None, 3]
+        num_anchors: num_actual_anchors / 3 - YOLOv3 detects at 3 different scales.
+                     The number of anchors are divided evenly into the 3 scales.
+        num_classes
+        """
+        # image_input = tf.placeholder(shape=(None, None, 3), dtype=tf.float32)
+        darknet_output = self.darknet_body(image_input)
+        x, y1 = self.make_last_layers(darknet_output, 512, num_anchors*(num_classes + 5), 1)
+        x = self.darknet_conv2d_bn_leaky(x, 256, (1, 1), layer_num="x_1_upsampling_0")
+        x = self.upsampling_2d_layer(x, 2, "x_1_upsampling_1")
+        print("Before first concat")
+        import pdb; pdb.set_trace()
+        x = self.concat_layer([x, self.layers[151]], -1, "x_1_concat")
+        import pdb; pdb.set_trace()
+        x, y2 = self.make_last_layers(x, 256, num_anchors * (num_classes + 5), 2)
+        x = self.darknet_conv2d_bn_leaky(x, 128, (1, 1), layer_num="x_2_upsampling_0")
+        x = self.upsampling_2d_layer(x, 2, "x_2_upsampling_1")
+        print("Before second concat")
+        import pdb; pdb.set_trace()
+        x = self.concat_layer([x, self.layers[91]], -1, "x_2_concat")
+        x, y3 = self.make_last_layers(x, 128, num_anchors * (num_classes + 5), 3)
+        import pdb;
+        pdb.set_trace()
+        return [y1, y2, y3]
+#         Need to concat layers now...
 
 
-def darknet_conv2d(input_layer, num_filters, kernel_size, layer_name, use_bias=True, strides=(1, 1), summary=False):
-    padding = 'valid' if strides == (2, 2) else 'same'
-    return conv_2d_layer(input_layer, num_filters, kernel_size, padding, None, 'glorot_uniform', layer_name,
-                         strides=strides, kernel_regularizer=l1_l2_reg(l2=5e-4), use_bias=use_bias, summary=summary)
-
-
-def darknet_conv2d_bn_leaky(input_layer, num_filters, kernel_size, strides=(1, 1), layer_num=None):
-    # if layer_num is not int(layer_num) or layer_num < 0:
-    #     raise("Layer num must be an int, and greater than 0")
-    darknet_conv = darknet_conv2d(input_layer, num_filters, kernel_size, "darknet_conv_{}".format(layer_num), False, strides)
-    batch_norm = batch_norm_layer(darknet_conv, -1, "batch_norm_{}".format(layer_num), summary=False)
-    return leaky_relu_layer(batch_norm, "leaky_relu_{}".format(layer_num), alpha=0.3, summary=False)
-
-
-def resblock_body(input_layer, num_filters, num_blocks, layer_num):
-    zero_pad = tf.pad(input_layer, paddings=((0, 0), (1, 0), (1, 0), (0, 0)), mode="CONSTANT")
-    print(zero_pad)
-    # zero_pad = tf.keras.layers.ZeroPadding2D()
-    leaky_darknet = darknet_conv2d_bn_leaky(zero_pad, num_filters, (3, 3), (2, 2), "{}_start".format(layer_num))
-    print(leaky_darknet)
-    current_network = leaky_darknet
-    for i in range(num_blocks):
-        conv_block = darknet_conv2d_bn_leaky(current_network, num_filters // 2, (1, 1), layer_num="{}_part_{}".format((layer_num + i), 1))
-        conv_block = darknet_conv2d_bn_leaky(conv_block, num_filters, (3, 3), layer_num="{}_part_{}".format((layer_num + i), 2))
-        current_network = tf.keras.layers.add([current_network, conv_block])
-        print(current_network)
-    return current_network
-
-
-def darknet_body(image_input):
-    x = darknet_conv2d_bn_leaky(image_input, 32, (3, 3), layer_num=0)
-    x = resblock_body(x, 64, 1, layer_num=1)
-    x = resblock_body(x, 128, 2, layer_num=2)
-    x = resblock_body(x, 256, 8, layer_num=4)
-    x = resblock_body(x, 512, 8, layer_num=12)
-    x = resblock_body(x, 1024, 4, layer_num=20)
-    return x
-
-
-def make_last_layers(input_layer, num_filters, num_outputs, output_identifier):
-    x = darknet_conv2d_bn_leaky(input_layer, num_filters, (1, 1), layer_num="x_output_{}_layer_{}".format(output_identifier, 0))
-    x = darknet_conv2d_bn_leaky(x, num_filters * 2, (3, 3), layer_num="x_output_{}_layer_{}".format(output_identifier, 1))
-    x = darknet_conv2d_bn_leaky(x, num_filters, (1, 1), layer_num="x_output_{}_layer_{}".format(output_identifier, 2))
-    x = darknet_conv2d_bn_leaky(x, num_filters * 2, (3, 3), layer_num="x_output_{}_layer_{}".format(output_identifier, 3))
-    x = darknet_conv2d_bn_leaky(x, num_filters, (1, 1), layer_num="x_output_{}_layer_{}".format(output_identifier, 4))
-
-    y = darknet_conv2d_bn_leaky(x, num_filters * 2, (3, 3), layer_num="y_output_{}_layer_{}".format(output_identifier, 0))
-    y = darknet_conv2d(y, num_outputs, (1, 1), layer_name="y_output_{}_layer_{}".format(output_identifier, 1))
-    return x, y
-
-
-def yolo_body(image_input, num_anchors, num_classes):
-    """
-    image_input: [None, None, 3]
-    num_anchors: num_actual_anchors / 3 - YOLOv3 detects at 3 different scales.
-                 The number of anchors are divided evenly into the 3 scales.
-    num_classes
-    """
-    # image_input = tf.placeholder(shape=(None, None, 3), dtype=tf.float32)
-    darknet_output = darknet_body(image_input)
-    x, y1 = make_last_layers(darknet_output, 512, num_anchors*(num_classes + 5), 1)
-    x = darknet_conv2d_bn_leaky(x, 256, (1, 1), layer_num="x_upsampling_0")
-    x = upsampling_2d_layer(x, "x_upsampling_1")
-
+def yolo_v3(image_input, num_anchors, num_classes):
+    # import tensorflow as tf; import model_pipeline_utils.tf_models.yolo_v3 as yolo3; image_input = tf.placeholder(shape=(1, 300, 300, 3), dtype=tf.float32)
+    # Image size must be a multiple of 32!!!  Otherwise this thing won't work...
+    return YOLOV3().yolo_body(image_input, num_anchors, num_classes)
