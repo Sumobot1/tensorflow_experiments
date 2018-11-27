f04d31d07ad9af79058ffecbf75de10a8e8cd622
Michael Peet <ubernopeavi@gmail.com>
2018-11-27 13:46:00 -0500

    Cleaned up yolov3 model file

--- model_pipeline_template/model_pipeline_utils/tf_models/yolo_v3.py
+++ model_pipeline_template/model_pipeline_utils/tf_models/yolo_v3.py
@@ -11,8 +11,10 @@ class YOLOV3:
         self.layers.append(layer)
         return layer
 
-    def darknet_conv2d(self, input_layer, num_filters, kernel_size, layer_name, use_bias=True, strides=(1, 1), summary=False):
-        return self.append_layer(darknet_conv2d(input_layer, num_filters, kernel_size, layer_name, use_bias, strides, summary))
+    def darknet_conv2d(self, input_layer, num_filters, kernel_size, layer_name,
+                       use_bias=True, strides=(1, 1), summary=False):
+        return self.append_layer(
+            darknet_conv2d(input_layer, num_filters, kernel_size, layer_name, use_bias, strides, summary))
 
     def batch_norm_layer(self, input_layer, axis_num, layer_name, summary=False):
         return self.append_layer(batch_norm_layer(input_layer, axis_num, layer_name, summary=summary))
@@ -33,20 +35,25 @@ class YOLOV3:
         return self.append_layer(concat_layer(values, axis, layer_name, summary))
 
     def darknet_conv2d_bn_leaky(self, input_layer, num_filters, kernel_size, strides=(1, 1), layer_num=None):
-        darknet_conv = self.darknet_conv2d(input_layer, num_filters, kernel_size, "darknet_conv_{}".format(layer_num), False, strides)
+        darknet_conv = self.darknet_conv2d(input_layer, num_filters, kernel_size,
+                                           "darknet_conv_{}".format(layer_num), False, strides)
         batch_norm = self.batch_norm_layer(darknet_conv, -1, "batch_norm_{}".format(layer_num), summary=False)
         return self.leaky_relu_layer(batch_norm, "leaky_relu_{}".format(layer_num), alpha=0.3, summary=False)
 
     def resblock_body(self, input_layer, num_filters, num_blocks, layer_num):
-        zero_pad = self.padding_layer(input_layer, paddings=((0, 0), (1, 0), (1, 0), (0, 0)), mode="CONSTANT", layer_name="pad_{}".format(layer_num))
+        zero_pad = self.padding_layer(input_layer, paddings=((0, 0), (1, 0), (1, 0), (0, 0)), mode="CONSTANT",
+                                      layer_name="pad_{}".format(layer_num))
         print(zero_pad)
         # zero_pad = tf.keras.layers.ZeroPadding2D()
-        leaky_darknet = self.darknet_conv2d_bn_leaky(zero_pad, num_filters, (3, 3), (2, 2), "{}_start".format(layer_num))
+        leaky_darknet = self.darknet_conv2d_bn_leaky(zero_pad, num_filters, (3, 3), (2, 2),
+                                                     "{}_start".format(layer_num))
         print(leaky_darknet)
         current_network = leaky_darknet
         for i in range(num_blocks):
-            conv_block = self.darknet_conv2d_bn_leaky(current_network, num_filters // 2, (1, 1), layer_num="{}_part_{}".format((layer_num + i), 1))
-            conv_block = self.darknet_conv2d_bn_leaky(conv_block, num_filters, (3, 3), layer_num="{}_part_{}".format((layer_num + i), 2))
+            conv_block = self.darknet_conv2d_bn_leaky(current_network, num_filters // 2, (1, 1),
+                                                      layer_num="{}_part_{}".format((layer_num + i), 1))
+            conv_block = self.darknet_conv2d_bn_leaky(conv_block, num_filters, (3, 3),
+                                                      layer_num="{}_part_{}".format((layer_num + i), 2))
             current_network = self.add_layer([current_network, conv_block])
             print(current_network)
         return current_network
@@ -61,14 +68,21 @@ class YOLOV3:
         return x
 
     def make_last_layers(self, input_layer, num_filters, num_outputs, output_identifier):
-        x = self.darknet_conv2d_bn_leaky(input_layer, num_filters, (1, 1), layer_num="x_{}_output_layer_{}".format(output_identifier, 0))
-        x = self.darknet_conv2d_bn_leaky(x, num_filters * 2, (3, 3), layer_num="x_{}_output_layer_{}".format(output_identifier, 1))
-        x = self.darknet_conv2d_bn_leaky(x, num_filters, (1, 1), layer_num="x_{}_output_layer_{}".format(output_identifier, 2))
-        x = self.darknet_conv2d_bn_leaky(x, num_filters * 2, (3, 3), layer_num="x_{}_output_layer_{}".format(output_identifier, 3))
-        x = self.darknet_conv2d_bn_leaky(x, num_filters, (1, 1), layer_num="x_{}_output_layer_{}".format(output_identifier, 4))
+        x = self.darknet_conv2d_bn_leaky(input_layer, num_filters, (1, 1),
+                                         layer_num="x_{}_output_layer_{}".format(output_identifier, 0))
+        x = self.darknet_conv2d_bn_leaky(x, num_filters * 2, (3, 3),
+                                         layer_num="x_{}_output_layer_{}".format(output_identifier, 1))
+        x = self.darknet_conv2d_bn_leaky(x, num_filters, (1, 1),
+                                         layer_num="x_{}_output_layer_{}".format(output_identifier, 2))
+        x = self.darknet_conv2d_bn_leaky(x, num_filters * 2, (3, 3),
+                                         layer_num="x_{}_output_layer_{}".format(output_identifier, 3))
+        x = self.darknet_conv2d_bn_leaky(x, num_filters, (1, 1),
+                                         layer_num="x_{}_output_layer_{}".format(output_identifier, 4))
 
-        y = self.darknet_conv2d_bn_leaky(x, num_filters * 2, (3, 3), layer_num="y_{}_output_layer_{}".format(output_identifier, 0))
-        y = self.darknet_conv2d(y, num_outputs, (1, 1), layer_name="y_{}_output_layer_{}".format(output_identifier, 1))
+        y = self.darknet_conv2d_bn_leaky(x, num_filters * 2, (3, 3),
+                                         layer_num="y_{}_output_layer_{}".format(output_identifier, 0))
+        y = self.darknet_conv2d(y, num_outputs, (1, 1),
+                                layer_name="y_{}_output_layer_{}".format(output_identifier, 1))
         return x, y
 
     def yolo_body(self, image_input, num_anchors, num_classes):
